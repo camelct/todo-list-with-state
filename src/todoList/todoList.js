3aba0e1648c53cbbf8c16c760d3a8eb623d7ffcc
chentuo <821982911@qq.com>
2019-07-25 19:26:21 +0800

    add search func

--- src/todoList/todoList.js
+++ src/todoList/todoList.js
@@ -3,32 +3,35 @@ import TodoItem from './todoItem/todoItem';
 import './todoList.css'
 
 let i = 0;
-
-
 export default class TodoList extends React.Component {
 
   state = {
-    list: [{id: 0, content: 'hehe', finish: true}],
+    list: [{ id: 0, content: 'hehe', finish: true }],
     value: '',
-    status:'all',
+    status: 'all',
+    searchText: ''
   };
 
   handleInput = (event) => {
-    this.setState({value: event.target.value})
+    this.setState({ value: event.target.value })
   };
 
+  onSearchInput = (event) => {
+    this.setState({ searchText: event.target.value })
+  }
+
   handleAdd = () => {
-    const {value, list} = this.state;
+    const { value, list } = this.state;
     if (value === '') {
       return
     }
-    const newList = [...list, {content: value, finish: false, id: ++i}];
-    this.setState({list: newList, value: ''});
+    const newList = [...list, { content: value, finish: false, id: ++i }];
+    this.setState({ list: newList, value: '' });
   };
 
   handleRemove = (id) => {
     const list = this.state.list.filter(item => item.id !== id);
-    this.setState({list})
+    this.setState({ list })
   };
 
   handleToggle = (id) => {
@@ -40,41 +43,52 @@ export default class TodoList extends React.Component {
       }
       return item;
     });
-    this.setState({list})
+    this.setState({ list })
   };
 
   handleStatus = (status) => {
-    this.setState({status})
+    this.setState({ status })
   };
 
   render() {
-    const {value, list, status} = this.state;
+    const { value, list, status, searchText } = this.state;
     return (
       <div className={'todo-list'}>
-        <input
-          type="text"
-          className={'todo-list-input'}
-          onChange={this.handleInput}
-          value={value.slice(0, 20)}
-        />
-        <button
-          className={'button'}
-          onClick={this.handleAdd}
-        >
-          新增
-        </button>
+        <div>
+          <input
+            type="text"
+            className={'todo-list-input'}
+            onChange={this.handleInput}
+            value={value.slice(0, 20)}
+          />
+          <button className={'button'} onClick={this.handleAdd}> 新增 </button>
+        </div>
+        <div>
+          <input
+            type="text"
+            className={'todo-list-input'}
+            onChange={this.onSearchInput}
+            value={searchText.slice(0, 20)}
+          />
+          <button className={'button'} onClick={this.handleSearch}> 搜索 </button>
+        </div>
         <div className={'button-group'}>
           <button onClick={this.handleStatus.bind(null, 'all')}>全部</button>
-          <button onClick={this.handleStatus.bind(null,'finished')}>已完成</button>
-          <button onClick={this.handleStatus.bind(null,'unfinishde')}>未完成</button>
+          <button onClick={this.handleStatus.bind(null, 'finished')}>已完成</button>
+          <button onClick={this.handleStatus.bind(null, 'unfinishde')}>未完成</button>
         </div>
         {
-          list.filter(item=>{
-            if(status === 'all'){
+          list.filter((item) => {
+            const text = searchText.replace(/\\+/g, '')
+            if (text === '') return true;
+            const pattrn = new RegExp(text + '', 'g');
+            return pattrn.test(item.content);
+          }).filter(item => {
+            if (status === 'all') {
               return true
-            }else if (status === 'finished') {
+            } else if (status === 'finished') {
               return item.finish
-            }else{
+            } else {
               return !item.finish
             }
           }).map(item =>
